36d1e0edacf51803c95dabe4eff577d68cbf69f3
gonzrr <miguelgchale@gmail.com>
2024-04-28 17:37:09 -0500

    chore:setup analitytics by vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 import { cn } from "@/lib/utils";
 import { Header } from "@/components/header";
@@ -41,6 +42,7 @@ export default function RootLayout({
             <Header />
             <main className="flex-1">{children}</main>
           </div>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
