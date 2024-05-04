4bea58b716d1fc0e435900f35a918d866ce311fc
gonzrr <miguelgchale@gmail.com>
2024-05-04 15:09:23 -0500

    add:dynamic og image

--- app/(sidebar-layout)/components/[...slug]/page.tsx
+++ app/(sidebar-layout)/components/[...slug]/page.tsx
@@ -1,6 +1,8 @@
 import { components } from "#site/content";
 import { MDXContent } from "@/components/mdx-components";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import { siteConfig } from "@/config/site";
 
 interface ComponentPageProps {
   params: { slug: string[] };
@@ -13,6 +15,45 @@ async function getComponentFromParams(params: ComponentPageProps["params"]) {
   );
 
   return component;
+}
+
+export async function generateMetadata({
+  params,
+}: ComponentPageProps): Promise<Metadata> {
+  const component = await getComponentFromParams(params);
+
+  if (!component) {
+    return {};
+  }
+
+  const ogSearchParams = new URLSearchParams();
+  ogSearchParams.set("title", component.title);
+
+  return {
+    title: component.title,
+    description: component.description,
+    authors: { name: siteConfig.author },
+    openGraph: {
+      title: component.title,
+      description: component.description,
+      type: "article",
+      url: component.slug,
+      images: [
+        {
+          url: `/api/og?${ogSearchParams.toString()}`,
+          width: 1200,
+          height: 630,
+          alt: component.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: component.title,
+      description: component.description,
+      images: [`/api/og?${ogSearchParams.toString()}`],
+    },
+  };
 }
 
 export async function generateStaticParams(): Promise<
