5d7785f46f150f38d79e369ef82c708ea335dd3e
Gonzalo Chale <miguelgchale@gmail.com>
2024-06-11 22:49:13 -0500

    style:change button variants bg color

--- components/examples/button.tsx
+++ components/examples/button.tsx
@@ -5,16 +5,16 @@ export default function Button() {
         <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#1e1e1c] to-[#10100F] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
         <span className="z-10 text-xs text-white/80">Default</span>
       </button>
-      <button className="relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#c7c7c7] to-[#F7F7F7] flex justify-center items-center border-2 border-[#F7F7F7] outline outline-2 outline-offset-2 outline-transparent focus:outline-[#c7c7c7]">
-        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#e4e4e4] to-[#F7F7F7] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
+      <button className="relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#d6d6d6] to-[#F7F7F7] flex justify-center items-center border-2 border-[#F7F7F7] outline outline-2 outline-offset-2 outline-transparent focus:outline-[#c7c7c7]">
+        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#e9e9e9] to-[#F7F7F7] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
         <span className="z-10 text-xs text-black/80">Secondary</span>
       </button>
-      <button className="group relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#d4ffe5] to-[#4dff94] flex justify-center items-center border-2 border-[#4dff94]  outline outline-2 outline-offset-2 outline-transparent focus:outline-[#d4ffe5]">
-        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#9effc5] to-[#4dff94] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
+      <button className="group relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#d4ffe5] to-[#3fe581] flex justify-center items-center border-2 border-[#3fe581]  outline outline-2 outline-offset-2 outline-transparent focus:outline-[#d4ffe5]">
+        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#8effbb] to-[#3fe581] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
         <span className="z-10 text-xs text-[#0C5132]">Success</span>
       </button>
-      <button className="group relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#FED3D1] to-[#ff9a97] flex justify-center items-center border-2 border-[#ff9a97]  outline outline-2 outline-offset-2 outline-transparent focus:outline-[#FED3D1]">
-        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#feb8b4] to-[#ff9a97] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
+      <button className="group relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#FED3D1] to-[#ff8682] flex justify-center items-center border-2 border-[#ff8682]  outline outline-2 outline-offset-2 outline-transparent focus:outline-[#FED3D1]">
+        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#feb8b4] to-[#ff8682] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
         <span className="z-10 text-xs text-[#8D1F0B]">Destructive</span>
       </button>
     </section>
