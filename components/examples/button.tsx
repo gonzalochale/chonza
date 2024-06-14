ec77a17697da644f5aabf5ef14af8c6b54e25d68
Gonzalo Chale <miguelgchale@gmail.com>
2024-06-13 22:26:26 -0500

    refactor:select button variant using props instead of copying individuals buttons

--- components/examples/button.tsx
+++ components/examples/button.tsx
@@ -1,22 +1,64 @@
-export default function Button() {
+import React from "react";
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "default" | "secondary" | "success" | "destructive";
+}
+
+export function Button({
+  children,
+  variant = "default",
+}: React.PropsWithChildren<ButtonProps>) {
+  const variants = {
+    default: {
+      background: "bg-gradient-to-b from-[#282828] to-[#10100F]",
+      border: "border-[#10100F]",
+      focusOutline: "focus:outline-[#282828]",
+      hoverBackground: "bg-gradient-to-b from-[#1e1e1c] to-[#10100F]",
+      textColor: "text-white/80",
+    },
+    secondary: {
+      background: "bg-gradient-to-b from-[#d6d6d6] to-[#F7F7F7]",
+      border: "border-[#F7F7F7]",
+      focusOutline: "focus:outline-[#c7c7c7]",
+      hoverBackground: "bg-gradient-to-b from-[#e9e9e9] to-[#F7F7F7]",
+      textColor: "text-black/80",
+    },
+    success: {
+      background: "bg-gradient-to-b from-[#d4ffe5] to-[#3fe581]",
+      border: "border-[#3fe581]",
+      focusOutline: "focus:outline-[#d4ffe5]",
+      hoverBackground: "bg-gradient-to-b from-[#8effbb] to-[#3fe581]",
+      textColor: "text-[#0C5132]",
+    },
+    destructive: {
+      background: "bg-gradient-to-b from-[#FED3D1] to-[#ff8682]",
+      border: "border-[#ff8682]",
+      focusOutline: "focus:outline-[#FED3D1]",
+      hoverBackground: "bg-gradient-to-b from-[#feb8b4] to-[#ff8682]",
+      textColor: "text-[#8D1F0B]",
+    },
+  };
+  const styles = variants[variant] || variants.default;
+
   return (
-    <section className="w-full h-full flex max-sm:flex-col justify-center items-center gap-10">
-      <button className="relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#282828] to-[#10100F] flex justify-center items-center border-2 border-[#10100F] outline outline-2 outline-offset-2 outline-transparent focus:outline-[#282828]">
-        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#1e1e1c] to-[#10100F] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
-        <span className="z-10 text-xs text-white/80">Default</span>
-      </button>
-      <button className="relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#d6d6d6] to-[#F7F7F7] flex justify-center items-center border-2 border-[#F7F7F7] outline outline-2 outline-offset-2 outline-transparent focus:outline-[#c7c7c7]">
-        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#e9e9e9] to-[#F7F7F7] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
-        <span className="z-10 text-xs text-black/80">Secondary</span>
-      </button>
-      <button className="group relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#d4ffe5] to-[#3fe581] flex justify-center items-center border-2 border-[#3fe581]  outline outline-2 outline-offset-2 outline-transparent focus:outline-[#d4ffe5]">
-        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#8effbb] to-[#3fe581] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
-        <span className="z-10 text-xs text-[#0C5132]">Success</span>
-      </button>
-      <button className="group relative min-w-[5rem] px-4 py-2 rounded-[12px] bg-gradient-to-b from-[#FED3D1] to-[#ff8682] flex justify-center items-center border-2 border-[#ff8682]  outline outline-2 outline-offset-2 outline-transparent focus:outline-[#FED3D1]">
-        <span className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] bg-gradient-to-b from-[#feb8b4] to-[#ff8682] group-hover:opacity-60 transition-all duration-300 ease-in-out"></span>
-        <span className="z-10 text-xs text-[#8D1F0B]">Destructive</span>
-      </button>
-    </section>
+    <button
+      className={`group relative min-w-[5rem] px-4 py-2 rounded-[12px] ${styles.background} flex justify-center items-center border-2 ${styles.border} outline outline-2 outline-offset-2 outline-transparent ${styles.focusOutline}`}
+    >
+      <span
+        className={`absolute w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-[0.5rem] ${styles.hoverBackground} group-hover:opacity-60 transition-all duration-300 ease-in-out`}
+      ></span>
+      <span className={`z-10 text-xs ${styles.textColor}`}>{children}</span>
+    </button>
+  );
+}
+
+export default function ButtonExample() {
+  return (
+    <div className="flex max-md:flex-col gap-10">
+      <Button>Default</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="destructive">Destructive</Button>
+    </div>
   );
 }
